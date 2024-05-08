cdfa9d29889c0ddcc2abc5e47dee61abe18d2bdd
Clement Glt <guillemot.clement@protonmail.com>
2024-05-08 17:47:49 +0200

    💄 Mise en place du content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,21 @@
+import styles from './Content.module.scss';
+
 function Content(){
-    return <div className="b2 fill">Content</div>;
+    return (
+        <div className="fill container p20">
+            <h1 className='my30'>Découvrez nos nouvelles recettes</h1>
+            <div className={`${styles.contentCard} card p20`}>
+                <div className={ styles.grid }>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Content;
