eb7baf4cea90daadbd45e35b1ead3381e049ded7
Clement Glt <guillemot.clement@protonmail.com>
2024-05-12 10:12:49 +0200

    Ajout de la barre de recherche

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,32 @@
 import styles from './Content.module.scss';
 import Recipe from './Recipe';
 import { data } from '../data/recipes';
+import { useState } from 'react';
 
 function Content(){
 
 		const recipes = data;
 
+		const [filter, setFilter] = useState('');
+
+		function handleInput(e){
+			const filter = e.target.value;
+			setFilter(filter.trim().toLowerCase());
+		}
+
 		return (
 				<div className="fill container p20">
 						<h1 className='my30 '>Découvrez nos nouvelles recettes</h1>
-						<div className={`${styles.contentCard} card p20`}>
-								<div className={ styles.grid }>
-										{recipes.map( r => (
-												<Recipe title={r.title} picture={r.picture}/>
-										))}
-								</div>
+						<div className={`${styles.contentCard} card p20 flex column `}>
+							<div className={`flex row justify-center align-center my30 ${styles.searchbar}`}>
+								<i className="fa-solid fa-magnifying-glass mr15"></i>
+								<input onInput={handleInput} className='fill' type="text" placeholder='Rechercher'/>
+							</div>
+							<div className={ styles.grid }>
+									{recipes.filter(r => r.title.toLowerCase().startsWith(filter)).map( r => (
+											<Recipe title={r.title} picture={r.picture}/>
+									))}
+							</div>
 						</div>
 				</div>
 		);
