c869927f8a7f5f599e026b82e43fe4b6b0215e6b
Clement Glt <guillemot.clement@protonmail.com>
2024-05-12 09:54:15 +0200

    Ajout du header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import styles from './Header.module.scss';
 import logo from '../assets/images/logo.png';
+import { useState } from 'react';
+import HeaderMenuXs from './HeaderMenuXs';
 
 function Header(){
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <header className={`${styles.header} flex row align-center justify-center`}>
-            <i className="fa-solid fa-bars mr15"></i>
+            
             <div className='fill'>
                 <img src={ logo } alt="logo" />
             </div>
-            <ul>
+            <ul className={styles.headerList}>
                 <button className='mr5 btn btn-secondary'>    
-                    <i className="fa-solid fa-basket-shopping mr5"></i>
+                    <i className="fa-solid fa-heart mr5"></i>
                     <span>Wishlist</span>
                 </button>
                 <button className='btn btn-primary'>Connexion</button>
             </ul>
+            <i onClick={ () => setShowMenu(true) } className={`fa-solid fa-bars ${styles.headerXs}`}></i>
+            { showMenu && 
+            <>
+                <div onClick={ () => setShowMenu(false) } className='calc'></div>
+                <HeaderMenuXs/>
+            </>
+             }
         </header>
     ); 
 }
